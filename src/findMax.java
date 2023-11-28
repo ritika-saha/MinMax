0de9bef7be8ca230ad56b29850b1917d6e30d40a
ritika-saha <ritika882001@gmail.com>
2023-11-28 21:10:40 +0530

    using generic class

--- src/findMax.java
+++ src/findMax.java
@@ -5,5 +5,17 @@ public class findMax<T extends Comparable<T>>  {
         this.ob2=b;
         this.ob3=c;
     }
-    
+    public static <T extends Comparable<T>> void largest(T a, T b, T c) {
+        if (a.compareTo(b) >= 0 && a.compareTo(c) >= 0) {
+            System.out.println(a + " is the largest");
+        } else if (b.compareTo(a) >= 0 && b.compareTo(c) >= 0) {
+            System.out.println(b + " is the largest");
+        } else {
+            System.out.println(c + " is the largest");
+        }
+    }
+
+    public void testMax(){
+        largest(ob1,ob2,ob3);
+    }
 }
--- src/minmaxmain.java
+++ src/minmaxmain.java
@@ -4,7 +4,6 @@ public class minmaxmain {
     public static void main(String[] args){
         System.out.println("welcome to test maximun");
 
-        System.out.println("welcome to test maximun");
         System.out.println("enter 3 integers");
         Scanner sc=new Scanner(System.in);
         
@@ -12,7 +11,8 @@ public class minmaxmain {
         Integer b=sc.nextInt();
         Integer c=sc.nextInt();
 
-        
+        findMax<Integer> intCmp=new findMax<>(a, b, c);
+        intCmp.testMax();
 
         System.out.println("enter 3 float numbers");
        
@@ -20,7 +20,8 @@ public class minmaxmain {
         Float b1=sc.nextFloat();
         Float c1=sc.nextFloat();
 
-        
+         findMax<Float> floatCmp=new findMax<>(a1, b1, c1);
+         floatCmp.testMax();
 
         System.out.println("enter 3 strings");
         sc.nextLine();
@@ -28,7 +29,8 @@ public class minmaxmain {
         String b2=sc.nextLine();
         String c2=sc.nextLine();
 
-        
+         findMax<String> strCmp=new findMax<>(a2, b2, c2);
+         strCmp.testMax();
 
 
         sc.close();
