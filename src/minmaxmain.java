1bfee297cb570be2728bcf803c86848ca4f77a6e
ritika-saha <ritika882001@gmail.com>
2023-11-28 20:30:34 +0530

    string comparison

--- src/minmaxmain.java
+++ src/minmaxmain.java
@@ -1,5 +1,28 @@
+import java.util.*;
+
 public class minmaxmain {
+    public static void largest(String a, String b, String c) {
+        if (a.compareTo(b) >= 0 && a.compareTo(c) >= 0) {
+            System.out.println(a + " is the largest");
+        } else if (b.compareTo(a) >= 0 && b.compareTo(c) >= 0) {
+            System.out.println(b + " is the largest");
+        } else {
+            System.out.println(c + " is the largest");
+        }
+    }
+    
     public static void main(String[] args){
         System.out.println("welcome to test maximun");
+        System.out.println("enter 3 strings");
+        Scanner sc=new Scanner(System.in);
+        
+        String a=sc.nextLine();
+        String b=sc.nextLine();
+        String c=sc.nextLine();
+
+        largest(a,b,c);
+
+
+        sc.close();
     }
 }
