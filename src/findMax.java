150d241d466a36344d8a40aeee1e5bd191ededa5
ritika-saha <ritika882001@gmail.com>
2023-11-28 20:55:01 +0530

    defining generic class

---
+++ src/findMax.java
@@ -0,0 +1,9 @@
+public class findMax<T extends Comparable<T>>  {
+    private T ob1,ob2,ob3;
+    public findMax(T a,T b,T c){
+        this.ob1=a;
+        this.ob2=b;
+        this.ob3=c;
+    }
+    
+}
--- src/minmaxmain.java
+++ src/minmaxmain.java
@@ -1,5 +1,36 @@
+import java.util.*;
+
 public class minmaxmain {
     public static void main(String[] args){
         System.out.println("welcome to test maximun");
+
+        System.out.println("welcome to test maximun");
+        System.out.println("enter 3 integers");
+        Scanner sc=new Scanner(System.in);
+        
+        Integer a=sc.nextInt();
+        Integer b=sc.nextInt();
+        Integer c=sc.nextInt();
+
+        
+
+        System.out.println("enter 3 float numbers");
+       
+        Float a1=sc.nextFloat();
+        Float b1=sc.nextFloat();
+        Float c1=sc.nextFloat();
+
+        
+
+        System.out.println("enter 3 strings");
+        sc.nextLine();
+        String a2=sc.nextLine();
+        String b2=sc.nextLine();
+        String c2=sc.nextLine();
+
+        
+
+
+        sc.close();
     }
 }
